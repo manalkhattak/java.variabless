d610f1df6afc43badd3b5c67298a5ad74b913072
manalkhattak <manalmasood828@gmail.com>
2023-11-12 15:44:35 +0500

    variables in javascript

---
+++ script.mjs
@@ -0,0 +1,29 @@
+// VARIABLES
+// let firstName = "MANAL"
+// let lastName = "MASOOD KHATTAK"
+// alert(firstName)
+// alert(lastName)
+// let newName = firstName.toLowerCase()
+// let oldName = lastName.toUpperCase()
+// alert(newName)
+// alert(oldName)
+
+// name = prompt("Enter Your Name:")
+// console.log("Length of your name is " + name.length)
+// if (name.length > 15){
+//     console.log ("You have a lengthy name")
+// }
+// else{
+//     console.log("You have a short name")
+// }
+
+// let num = prompt("Enter a number:")
+// if (num % 2==0){
+//     console.log("The number you entered is Even")
+// }
+// else{
+//     console.log("The number you entered is Odd")
+// }
+let paragraph = prompt("Tell me about yourself")
+console.log("Length of your paragraph is " + paraghraph.length)
+if(paragraph.length)
